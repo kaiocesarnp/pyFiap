6868edaa878e678a1157ebd9f62276b1ffc554a3
kaiocesarnp <kaiocesarnp@gmail.com>
2023-04-04 17:46:23 -0300

    pyfiap

--- aula1/variavies.py
+++ aula1/variavies.py
@@ -10,3 +10,4 @@ print("O tipo de dado da variavel [nome] é: ",type(nome))
 print("O tipo de dado da variavel [empresa] é: ",type(empresa))
 print("O tipo de dado da variavel [qtde_funcionarios] é: ",type(qtde_funcionarios))
 print("O tipo de dado da variavel [mediaMensalidade] é: ",type(mediaMensalidade))
+
