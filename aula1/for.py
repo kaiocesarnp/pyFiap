46c5c65ec85744056d5b14d79a267b47398f23ca
kaiocesarnp <kaiocesarnp@gmail.com>
2023-04-05 17:53:21 -0300

    pyfiap

---
+++ aula1/for.py
@@ -0,0 +1,4 @@
+tabuada=int(input("Digite um número para exibir a tabuada: "))
+print("Tabuada do número ", tabuada)
+for valor in range(1,11,1): #1,11,1 = inicio, fim e incremento
+    print(str(tabuada) + " x " + str(valor) + " = " + str((tabuada*valor)))
