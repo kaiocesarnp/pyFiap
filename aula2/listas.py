e45ddb7e4fb3fcb66bcd0b0760f6aa683e645af5
kaiocesarnp <kaiocesarnp@gmail.com>
2023-04-07 20:02:55 -0300

    pyfiap

--- aula2/listas.py
+++ aula2/listas.py
@@ -1,11 +1,12 @@
 inventario=[]
 resposta="S"
 while resposta=="S":
-  inventario.append(input("Equipamento: "))
+  inventario.append(input("Equipamento: ")) #append = inserido na lista 'inventario'
   inventario.append(float(input("Valor: ")))
   inventario.append(int(input("Número Serial: ")))
   inventario.append(input("Departamento: "))
-  resposta=input("Digite "S" para continuar: ").upper()
+  resposta=input("Digite \"S\" para continuar: ").upper()  #upper = letra maiuscula, \"\" barras com aspas = aspas aparecem no print
 
 for elemento in inventario:
   print(elemento)
+
