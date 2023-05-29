392833d36375a4c98e80f7642bfd0d70fbec651e
kaiocesarnp <kaiocesarnp@gmail.com>
2023-05-29 17:51:14 -0300

    pyfiap

---
+++ aula4Cap5/primeiroArquivo.py
@@ -0,0 +1,22 @@
+# Criar arquivos:
+
+# arquivo = open("aula4Cap5/primeiro_arquivo.txt", "w")
+# arquivo.write("Meu primeiro arquivo!")
+# arquivo.close()
+
+
+# Melhorando o código, adicionando texto
+with open("aula4Cap5/primeiro_arquivo.txt", "a") as arquivo:
+    arquivo.write("\nHakuna Matata")
+
+
+
+
+
+# r = read, ler
+# w = write, escrita
+# a = append, ler e escrever
+# x = exclusivo, ninguem pode manipular enquanto voce está com o arq. aberto
+
+# with = com a abertura deste arqvuivo
+#("\n") = pular linha
