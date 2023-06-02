36f1bac313f5cbf2647894cc28bf87587e5d941e
kaiocesarnp <kaiocesarnp@gmail.com>
2023-06-02 08:58:06 -0300

    pyfiap

--- aula4Cap5/textos.py
+++ aula4Cap5/textos.py
@@ -1,2 +1,7 @@
 texto = "Leia e Han Solo"
+    # 012345678901234 = posições do caracteres. Por exemplo, o primeiro espaço é o 4 pq a contagem começa no 0(zero)
 print(texto[0:4])
+print(texto[7:]) #a partir dos dois pontos sem determinar outro numero após eles, a contagem vai até o final
+
+
+
