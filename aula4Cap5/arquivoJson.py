c8db2c8e1bd58a116692415cb0902cf93d417274
kaiocesarnp <kaiocesarnp@gmail.com>
2023-06-02 12:04:30 -0300

    pyfiap

---
+++ aula4Cap5/arquivoJson.py
@@ -0,0 +1,10 @@
+import json
+
+# lendo o arquivo:
+# with open("aula4Cap5/bd.json", "r") as arq_json: #abrindo o arquivo
+#     dicionario = json.load(arq_json) # utilizando o módulo 'json' / carregando(load) o arquivo em "dicionario"
+#     for chave,dados in dicionario.items():
+#         print(chave + " | " + str(dados))
+
+ 
+#Gravando o arquivo:
