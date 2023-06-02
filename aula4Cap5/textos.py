abe5470e22b93b1b9f28e56942710c84861543d0
kaiocesarnp <kaiocesarnp@gmail.com>
2023-06-02 09:00:50 -0300

    pyfiap

--- aula4Cap5/textos.py
+++ aula4Cap5/textos.py
@@ -1,7 +1,11 @@
 texto = "Leia e Han Solo"
     # 012345678901234 = posições do caracteres. Por exemplo, o primeiro espaço é o 4 pq a contagem começa no 0(zero)
+    
 print(texto[0:4])
 print(texto[7:]) #a partir dos dois pontos sem determinar outro numero após eles, a contagem vai até o final
+
+# 543210987654321 - = contagem regressiva, ao contrário, utilizando o sinal de menos
+print(texto[-8:])
 
 
 
