ec0693788115a1117dcdf8c4d8de26deec7ba2bc
kaiocesarnp <kaiocesarnp@gmail.com>
2023-06-10 10:34:38 -0300

    pyfiap

--- aula6Cap7/cliente_udp.py
+++ aula6Cap7/cliente_udp.py
@@ -1,12 +1,17 @@
 # APLICAÇÃO CLIENTE COM UDP
+# Faz ligação com o arquivo "servido_udp.py"
+# Rodar primeiro o servidor_tcp.py e depois este arquivo.
+# Funcionou atraves do debug. Procurando outras formas...
 
 from socket import *
 
 servidor="127.0.0.1"
 porta=43210
+
 obj_socket = socket(AF_INET, SOCK_DGRAM)
 obj_socket.connect((servidor, porta))
 saida=""
+
 while saida!="X":
     msg = input("Sua mensagem: ")
     obj_socket.sendto(msg.encode(), (servidor,porta))
